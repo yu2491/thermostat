59edf13b9b05721b531ea9d3e8dcb4fbc58ebc05
Yasmin Underdown <yunderdown1@gmail.com>
2020-09-04 09:44:38 +0100

    tests passing, logic done

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,6 +8,85 @@ describe('Thermostat:', function() {
   });
 
   it('is initially set to 20 degrees', function() {
-    expect(thermostat.temperature()).toEqual(20);
+    expect(thermostat.getTemperature()).toEqual(20);
   });
+
+  it('can increase temperateure with increase function', function(){
+    thermostat.increase();
+    expect(thermostat.getTemperature()).toEqual(21);
+  });
+
+  it('can decrease temperateure with decrease function', function(){
+    thermostat.decrease();
+    expect(thermostat.getTemperature()).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degrees', function() {
+    for(var i=0; i<11; i++) {
+      thermostat.decrease();
+    }
+    expect(thermostat.getTemperature()).toEqual(10);
+  });
+
+  it('has a  default of powersaving being on', function() {
+    expect(thermostat.isPowerSavingOn()).toBe(true);
+  });
+
+  it('powersaving can be turned off', function() {
+    thermostat.switchPowerSavingOff();
+    expect(thermostat.isPowerSavingOn()).toBe(false);
+  });
+
+  it('powersaving can be turned on', function() {
+    thermostat.switchPowerSavingOff();
+    thermostat.switchPowerSavingOn();
+    expect(thermostat.isPowerSavingOn()).toBe(true);
+  });
+
+  it('has a max temp of 25 if power saving mode on', function() {
+    for(var  i=0; i<6; i++)  {
+      thermostat.increase();
+    }
+    expect(thermostat.getTemperature()).toEqual(25);
+  });
+
+  it('has a max temp of 32 if power saving mode off', function() {
+    thermostat.switchPowerSavingOff();
+    for(var  i=0; i<13; i++)  {
+      thermostat.increase();
+    }
+    expect(thermostat.getTemperature()).toEqual(32);
+  });
+
+  it('reset temp to 20', function() {
+    for(var  i=0; i<6; i++)  {
+      thermostat.increase();
+    }
+    thermostat.reset();
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+
+  describe('usage levels', function() {
+    it('return low-usage', function() {
+      for(var  i=0; i<4; i++)  {
+        thermostat.decrease();
+      }
+      expect(thermostat.usage()).toEqual('low-usage')
+    });
+
+    it('return medium-usage', function() {
+      expect(thermostat.usage()).toEqual('medium-usage')
+    });
+
+    it('return high-usage', function() {
+      thermostat.powersaving = false
+      for(var i=0; i<7; i++)  {
+        thermostat.increase();
+      }
+      expect(thermostat.usage()).toEqual('high-usage')
+    });
+  });
+
+
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,1 +1,74 @@
 'use strict'
+
+class Thermostat {
+
+
+
+  constructor(){
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+    this.powersaving = true;
+    this.MEDIUM_USAGE_MIN = 18;
+    this.MEDIUM_USAGE_MAX = 25;
+  }
+
+  getTemperature(){
+    return this.temperature;
+  }
+
+  increase(){
+    if(this.isMaximumTemperature()){
+     return;
+    }
+    this.temperature++;
+  }
+
+  decrease(){
+    if(this.isMinimumTemperature()){
+      return;
+    }
+    this.temperature--;
+  }
+
+  isMinimumTemperature(){
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isMaximumTemperature(){
+    if(this.isPowerSavingOn() === false) {
+      return this.temperature  === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
+  isPowerSavingOn(){
+    return this.powersaving === true;
+  }
+
+  switchPowerSavingOff(){
+    this.powersaving =  false;
+  }
+
+  switchPowerSavingOn(){
+    this.powersaving =  true;
+  }
+
+  reset(){
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  usage(){
+    if(this.temperature < this.MEDIUM_USAGE_MIN) {
+      return "low-usage"
+    } else if (this.temperature <= this.MEDIUM_USAGE_MAX) {
+      return "medium-usage"
+    } else {
+      return "high-usage"
+    }
+  }
+
+
+}
